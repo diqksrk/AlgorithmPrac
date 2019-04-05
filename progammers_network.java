762901ea4af4a693b00f6f70e5ca28c11d52f1cc
diqksrk <31757314+diqksrk@users.noreply.github.com>
2019-04-05 12:08:24 +0900

    Create progammers_network.java

---
+++ progammers_network.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+	static void dfs(int n, int[][] computers, boolean[] visited, int current){
+		visited[current]=true;
+		for (int i=0; i<n; i++){
+			boolean tf=visited[i];
+			int tfi=computers[current][i];
+			if (!visited[i] && computers[current][i]>0){
+				dfs(n,computers, visited, i);
+			}
+		}
+	}
+	
+	
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited=new boolean[n];
+        
+        for (int i=0; i<n; i++){
+        	if (!visited[i]){
+        		answer++;
+            	dfs(n, computers, visited, i);
+        	}
+        }
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+    	Solution sol=new Solution();
+    	int n=3;
+    	int[][] computers={{1,1,0}, {1,1,0}, {0,0,1}};
+    	sol.solution(n, computers);
+	}
+}
